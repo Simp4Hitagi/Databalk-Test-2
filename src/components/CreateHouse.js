50912f5241fab36d5e11c9ced2de393a546c728a
Simp4Hitagi <denorauten@gmail.com>
2023-06-04 18:59:20 +0200

    user and house render, add and update work, need to fix delete and add styling

--- src/components/CreateHouse.js
+++ src/components/CreateHouse.js
@@ -1,66 +1,209 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const CreateUserForm = () => {
-  const [emailAddress, setEmailAddress] = useState('');
-  const [userPassword, setUserPassword] = useState('');
-  const [userName, setUserName] = useState('');
+const CreateHouse = () => {
+  const [houseDescription, setHouseDescription] = useState('');
+  const [location, setLocation] = useState('');
+  const [price, setPrice] = useState('');
+  const [imgURL, setImgURL] = useState('');
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    const userData = {
-      emailAddress,
-      userPassword,
-      userName
+    const houseData = {
+      houseDescription,
+      location,
+      price,
+      imgURL
     };
 
     try {
-      const result = await axios.post('http://localhost:5000/register', userData);
-      console.log('New user created:', result.results);
+      const result = await axios.post('http://localhost:5000/house', houseData);
+      console.log('New house created:', result.results);
     } catch (error) {
-      console.error('Error creating user:', error);
+      console.error('Error creating house:', error);
     }
   };
 
   return (
     <div>
-      <h1>Create A New User</h1>
+      <h1>Create A New House</h1>
       <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="emailAddress">Email Address:</label>
-          <input
-            type="email"
-            id="emailAddress"
-            value={emailAddress}
-            onChange={(e) => setEmailAddress(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="userPassword">User Password:</label>
-          <input
-            type="password"
-            id="userPassword"
-            value={userPassword}
-            onChange={(e) => setUserPassword(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="userName">User Name:</label>
+          <label htmlFor="houseDescription">House Description:</label>
           <input
             type="text"
-            id="userName"
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
+            id="houseDescription"
+            value={houseDescription}
+            onChange={(e) => setHouseDescription(e.target.value)}
             required
           />
         </div>
-        <button type="submit">Create User</button>
+        <div>
+          <label htmlFor="location">Location:</label>
+          <input
+            type="text"
+            id="location"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="price">Price:</label>
+          <input
+            type="number"
+            id="price"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="imgURL">Image Link:</label>
+          <input
+            type="text"
+            id="imgURL"
+            value={imgURL}
+            onChange={(e) => setImgURL(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Create product</button>
       </form>
     </div>
   );
 };
 
-export default CreateUserForm;
+export default CreateHouse;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+
+// const CreateHouse = () => {
+//   const [houseID, setHouseID] = useState('');
+//   const [houseDescription, setHouseDescription] = useState('');
+//   const [location, setLocation] = useState('');
+//   const [price, setPrice] = useState('');
+//   const [imgURL, setImgURL] = useState('');
+//   const [houses, setHouses] = useState([]);
+
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
+
+//     const houseData = {
+//         houses,
+//       houseID,
+//       houseDescription,
+//       location,
+//       price,
+//       imgURL
+//     };
+
+//     try {
+//       const result = await axios.post('http://localhost:5000/house', houseData);
+//       console.log('New house created:', result.data.results);
+//     } catch (error) {
+//       console.error('Error creating a house:', error);
+//     }
+//   };
+
+//   const fetchHousesData = async () => {
+//     try {
+//       const response = await axios.get('http://localhost:5000/houses');
+//       if (response.status === 200) {
+//         console.log(response.data.results);
+
+//         setHouses(response.data.results);
+//       }
+//     } catch (error) {
+//       console.error('Error loading house data:', error);
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchHousesData();
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Create A New House</h1>
+      
+//       <form onSubmit={handleSubmit}>
+//         <div>
+//           <label htmlFor="houseID">House ID:</label>
+//           <input
+//             type="number"
+//             id="houseID"
+//             value={houseID}
+//             onChange={(e) => setHouseID(e.target.value)}
+//           />
+//         </div>
+//         <div>
+//           <label htmlFor="houseDescription">House Description:</label>
+//           <input
+//             type="text"
+//             id="houseDescription"
+//             value={houseDescription}
+//             onChange={(e) => setHouseDescription(e.target.value)}
+//           />
+//         </div>
+//         <div>
+//           <label htmlFor="location">House Location:</label>
+//           <input
+//             type="text"
+//             id="location"
+//             value={location}
+//             onChange={(e) => setLocation(e.target.value)}
+//           />
+//         </div>
+//         <div>
+//           <label htmlFor="price">Price:</label>
+//           <input
+//             type="number"
+//             id="price"
+//             value={price}
+//             onChange={(e) => setPrice(e.target.value)}
+//           />
+//         </div>
+//         <div>
+//           <label htmlFor="imageURL">Image Link:</label>
+//           <input
+//             type="text"
+//             id="imgURL"
+//             value={imgURL}
+//             onChange={(e) => setImgURL(e.target.value)}
+//           />
+//         </div>
+//         <button type="submit">Create New House</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default CreateHouse;
